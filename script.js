ca65344e9fb9046732ca6c3a74c639c882f6e9b1
Haris362 <haris.tariq423@gmail.com>
2025-06-29 23:57:04 +0500

    reload the website code

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // Container where input and buttons present
 const container = document.createElement("div");
 document.body.appendChild(container);
+container.classList.add("container")
 // Input where you can enter the number
 const input = document.createElement("input");
 input.placeholder = `Enter the number`
 container.appendChild(input)
+input.classList.add("input")
 // Enter button
 const button = document.createElement("button");
 button.textContent = "Enter";
 container.appendChild(button)
+button.classList.add("enter-Button")
 
 // Random number created
 const randomNum = Math.random()*100;
@@ -17,10 +20,16 @@ console.log(fixedNum);
 
 //Message div
 const tryAgain_Container = document.createElement("div");
+tryAgain_Container.classList.add("message-Container")
 // Message
 const message =  document.createElement("p");
+message.classList.add("message")
 // Try Again Button
 const tryButton = document.createElement("button");
+tryButton.classList.add("try-Button")
+tryButton.addEventListener("click",function(){
+    window.location.reload();
+})
 
 button.addEventListener("click",function(){
     const value = input.value;
@@ -35,6 +44,7 @@ button.addEventListener("click",function(){
         // Try button
         tryAgain_Container.appendChild(tryButton);
         tryButton.textContent = "Next"
+        
     }
     else if (value !== fixedNum){
         console.log("NO NO");
@@ -50,8 +60,8 @@ button.addEventListener("click",function(){
     input.value = ""
 })
 
-const value = input.value;
 input.addEventListener("keydown",function(e){
+    const value = input.value;
     if (e.key === "Enter"){
         if (value == fixedNum){
         console.log("great");
